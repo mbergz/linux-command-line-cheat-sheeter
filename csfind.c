0c7c99e83bea915a00e377c36b35c41febbadd62
Martin Bergström <emarbe02@gmail.com>
2024-05-10 11:27:09 +0200

    Break out functions of enable/reset non-canonical mode

--- csfind.c
+++ csfind.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <termios.h>
 
-
 #define MAX_OPTIONS 3
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RESET "\x1b[0m"
@@ -25,15 +24,19 @@ void printDir()
     printf("printing dir");
 }
 
-
-void printOptions(const char *options[], int selectedOption) {
+void printOptions(const char *options[], int selectedOption)
+{
     printf("\r\033[2K"); // Clear entire line
 
     // Print options with current selection highlighted
-    for (int i = 0; i < MAX_OPTIONS; ++i) {
-        if (i == selectedOption) {
+    for (int i = 0; i < MAX_OPTIONS; ++i)
+    {
+        if (i == selectedOption)
+        {
             printf("\x1b[7m*%s\x1b[0m ", options[i]); // Print highlighted option
-        } else {
+        }
+        else
+        {
             printf("%s ", options[i]); // Print regular option
         }
     }
@@ -41,47 +44,62 @@ void printOptions(const char *options[], int selectedOption) {
     fflush(stdout); // Flush output buffer to ensure it's printed immediately
 }
 
-void printFindCheatSheet()
+void enableNonCanonicalMode(struct termios *old)
 {
-    const char *options[MAX_OPTIONS] = {"all", "file", "dir"};
-    int selectedOption = 0;
-    char c;
-
-    // Set terminal into non-canonical mode
-    struct termios old;
+    tcgetattr(0, old);
     struct termios new;
-    tcgetattr(0, &old);
-    new = old;
-    //c_lflag = local mode
+    new = *old;
+    // c_lflag = local mode
     // ICANON = canonincal mode
     // ECHO = echos input to terminal
     new.c_lflag &= ~(ICANON | ECHO);
     // TCSANOW means apply changes immediately
     tcsetattr(0, TCSANOW, &new);
+}
 
-    while (1) {
+void resetTerminalMode(struct termios *old)
+{
+    tcsetattr(0, TCSANOW, old);
+}
+
+void printFindCheatSheet()
+{
+    struct termios old;
+
+    const char *options[MAX_OPTIONS] = {"all", "file", "dir"};
+    int selectedOption = 0;
+    char c;
+
+    enableNonCanonicalMode(&old);
+
+    while (1)
+    {
         printOptions(options, selectedOption);
 
         // Read a single character
-        if (read(STDIN_FILENO, &c, 1) == -1) {
+        if (read(STDIN_FILENO, &c, 1) == -1)
+        {
             perror("read");
-            //exit(1);
+            // exit(1);
         }
 
         // Process user input
-        if (c == '\t') {
+        if (c == '\t')
+        {
             selectedOption = (selectedOption + 1) % MAX_OPTIONS; // Move to next option
             printOptions(options, selectedOption);
-        } else if (c == 'a') {
+        }
+        else if (c == 'a')
+        {
             printf("The 'a' key was pressed.\n");
-        } else if (c == '\n') {
+        }
+        else if (c == '\n')
+        {
             // Exit loop on Enter key
             break;
         }
     }
 
-    // Reset terminal
-    tcsetattr(0, TCSANOW, &old);
+    resetTerminalMode(&old);
     printf("\nSelected option: %s\n", options[selectedOption]);
-
 }
