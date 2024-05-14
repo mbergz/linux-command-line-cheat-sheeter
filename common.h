32a0e9425580403b41769f4e5ac612f7494ccf2a
Martin Bergström <emarbe02@gmail.com>
2024-05-14 19:26:12 +0200

    Add missing changes to common.h

--- common.h
+++ common.h
@@ -1,10 +1,11 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-
-struct CommandInfo {
-    const char* command;
-    const char* description;
+struct CommandInfo
+{
+    const char *command;
+    const char *description;
+    int editIndexes[5];
 };
 
 typedef struct CommandInfo CommandInfo;
