364d8c7aadf12eb047bd39afd2026fd3d08b2aac
Martin Bergström <emarbe02@gmail.com>
2024-08-16 11:14:00 +0200

    Add command to sed

--- src/sed.c
+++ src/sed.c
@@ -4,6 +4,7 @@
 
 static CommandInfo sedCommands[] = {
     {"sed 's/unix/linux/' file.txt", "Replace the FIRST word occurence of unix with linux in file.txt", {11, 17, 28}},
+    {"sed 's/unix/linux/i' file.txt", "Replace the FIRST word occurence and ignore case sensitive of unix with linux in file.txt", {11, 17, 29}},
     {"sed 's/unix/linux/g' file.txt", "Replace ALL word occurence of unix with linux in file.txt", {11, 17, 29}},
     {"sed 's/unix/linux/3' file.txt", "Replace the 3 word occurence of unix with linux in file.txt", {11, 17, 19, 29}},
 };
