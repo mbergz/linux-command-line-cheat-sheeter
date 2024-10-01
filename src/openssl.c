294c5f74d5798213271367fbdb03a55c63bfa274
Martin Bergström <emarbe02@gmail.com>
2024-10-01 18:33:49 +0200

    Add openssl cmd for viewing csr

--- src/openssl.c
+++ src/openssl.c
@@ -8,6 +8,7 @@ static CommandInfo opensslCommands[] = {
     {"openssl x509 -in mycert.pem -text -noout", "View x509 certificate", {28}},
     {"openssl s_client -showcerts -connect www.example.com:443 </dev/null", "View x509 cert from website", {52}},
     {"openssl s_client -showcerts -servername www.example.com -connect www.example.com:443 </dev/null", "View x509 cert from website with SNI", {}},
+    {"openssl req -in file.csr -text -noout", "View CSR", {24}},
     {"openssl genrsa -out my_private_key.key 2048", "Generate private key", {}},
     {"openssl rsa -in my_private_key.key -pubout > my_public_key.pub", "Generate public key with private key", {}},
     {"openssl req -x509 -new -key my_private_key.key -days 365 -out mycert.pem", "Generate self-signed certificate", {}},
